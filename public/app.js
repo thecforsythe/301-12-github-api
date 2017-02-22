c4d13c88fc0439a2d56280ea9727804aa34e1f87
Sam Hamm <samhamm@gmail.com>
2017-02-21 16:43:44 -0800

    after classssssss

--- public/app.js
+++ public/app.js
@@ -1,8 +1,15 @@
 // Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
 
 $.ajax({
-  // go get the data
+  url: 'https://api.github.com/user/repos',
+  method: 'GET',
+  headers: {
+	   Authorization: `token ${githubToken}`}
 })
-.then(
-  // render the data
-);
+.then(data => {
+  console.log(data);
+  data.forEach(repo => $('#results').append(`<p>${repo.name}</p>`));
+},
+  err => {
+    console.error('Status Code: ', err.status);
+  });
